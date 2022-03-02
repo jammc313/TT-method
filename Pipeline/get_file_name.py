4a879afdea455a4a8f9b1d3bffa8d0ad693b532e
James McK <43877269+jammc313@users.noreply.github.com>
2022-03-02 15:34:43 +0100

    Update get_file_name.py

--- Pipeline/get_file_name.py
+++ Pipeline/get_file_name.py
@@ -1,19 +1,23 @@
 def get_group_name_dict():
     b_dict={'Mbuti':[]}
-    b_dict['Mbuti'].append(('Mbuti1','Mbuti1.AllSites.A3.vqsred.vcf.gz'))
-    b_dict['Mbuti'].append(('Mbuti2','Mbuti2.AllSites.A3.vqsred.vcf.gz'))
-    b_dict['Mbuti'].append(('Mbuti3','Mbuti3.AllSites.A3.vqsred.vcf.gz'))
+    b_dict['Mbuti'].append(('Mbuti1','Mbuti1.AllSites.A3.vqsred_chr.vcf.gz'))
+    b_dict['Mbuti'].append(('Mbuti2','Mbuti2.AllSites.A3.vqsred_chr.vcf.gz'))
 
     b_dict.update({'French':[]})
-    b_dict['French'].append(('French1','French1.AllSites.A3.vqsred.vcf.gz'))
-    b_dict['French'].append(('French2','French2.AllSites.A3.vqsred.vcf.gz'))
-    b_dict['French'].append(('French3','French3.AllSites.A3.vqsred.vcf.gz'))
+    b_dict['French'].append(('French1','French1.AllSites.A3.vqsred_chr.vcf.gz'))
+    b_dict['French'].append(('French2','French2.AllSites.A3.vqsred_chr.vcf.gz'))
 
     b_dict.update({'Papuan':[]})	
-    b_dict['Papuan'].append(('Papuan1','Papuan1.AllSites.A3.vqsred.vcf.gz'))
-    b_dict['Papuan'].append(('Papuan2','Papuan2.AllSites.A3.vqsred.vcf.gz'))
-    b_dict['Papuan'].append(('Papuan8','Papuan8.AllSites.A3.vqsred.vcf.gz'))
-
+    b_dict['Papuan'].append(('Papuan1','Papuan1.AllSites.A3.vqsred_chr.vcf.gz'))
+    b_dict['Papuan'].append(('Papuan2','Papuan2.AllSites.A3.vqsred_chr.vcf.gz'))
+    
+    b_dict.update({'Archaic':[]})       
+    b_dict['Archaic'].append(('Neanderthal','AltaiNea.hg19_1000g.dq.bqual.RG.realn-snpAD_chr.vcf.gz'))
+    b_dict['Archaic'].append(('Denisovan','T_hg19_1000g.bqual.RG.realn-snpAD_chr.vcf.gz'))
+    
+    b_dict.update({'BaalitoBay':[]})    
+    b_dict['BaalitoBay'].append(('baa001','baa001-dr_90perc_libr_160609.merge.hs37d5.fa.bqual.RG.realn.UG.recode_chr.vcf.gz'))
+    
     return b_dict
 
 def get_name_file_dict():
